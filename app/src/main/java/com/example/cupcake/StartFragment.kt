f9d6e0f94a4532a98273e7c868a413e7707d0121
Brav9 <vskh93.009@gmail.com>
2022-09-22 15:17:52 +0300

    Добавление метода hasNoFlavorSet() в класс OrderViewModel, для проверки наличия установленного вкуса для заказа.

--- app/src/main/java/com/example/cupcake/StartFragment.kt
+++ app/src/main/java/com/example/cupcake/StartFragment.kt
@@ -61,7 +61,11 @@ class StartFragment : Fragment() {
      * Start an order with the desired quantity of cupcakes and navigate to the next screen.
      */
     fun orderCupcake(quantity: Int) {
-       findNavController().navigate(R.id.action_startFragment_to_flavorFragment)
+        sharedViewModel.setQuantity(quantity)
+        if (sharedViewModel.hasNoFlavorSet()) {
+            sharedViewModel.setFlavor(getString(R.string.vanilla))
+        }
+        findNavController().navigate(R.id.action_startFragment_to_flavorFragment)
     }
 
     /**
